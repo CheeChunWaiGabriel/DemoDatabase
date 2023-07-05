ad77e735b2854c398745695eaa154e77d68499c8
22022050 <22022050@myrp.edu.sg>
2023-07-05 12:14:40 +0800

    Added some functionality

--- app/src/main/java/com/myapplicationdev/android/demodatabase/DBHelper.java
+++ app/src/main/java/com/myapplicationdev/android/demodatabase/DBHelper.java
@@ -1,13 +1,64 @@
 package com.myapplicationdev.android.demodatabase;
 
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.util.Log;
 
 import androidx.annotation.Nullable;
 
+import java.util.ArrayList;
+
 public class DBHelper extends SQLiteOpenHelper {
+    public void insertTask(String description, String date){
+
+        // Get an instance of the database for writing
+        SQLiteDatabase db = this.getWritableDatabase();
+        // We use ContentValues object to store the values for
+        //  the db operation
+        ContentValues values = new ContentValues();
+        // Store the column name as key and the description as value
+        values.put(COLUMN_DESCRIPTION, description);
+        // Store the column name as key and the date as value
+        values.put(COLUMN_DATE, date);
+        // Insert the row into the TABLE_TASK
+        db.insert(TABLE_TASK, null, values);
+        // Close the database connection
+        db.close();
+    }
+
+    public ArrayList<String> getTaskContent() {
+        // Create an ArrayList that holds String objects
+        ArrayList<String> tasks = new ArrayList<String>();
+        // Get the instance of database to read
+        SQLiteDatabase db = this.getReadableDatabase();
+        String[] columns = {COLUMN_ID, COLUMN_DESCRIPTION, COLUMN_DATE};
+        // Run the query and get back the Cursor object
+        Cursor cursor = db.query(TABLE_TASK, columns, null, null, null, null, null, null);
+
+        // moveToFirst() moves to first row, null if no records
+        if (cursor.moveToFirst()) {
+            // Loop while moveToNext() points to next row
+            //  and returns true; moveToNext() returns false
+            //  when no more next row to move to
+            do {
+                // Add the task content to the ArrayList object
+                //  getString(0) retrieves first column data
+                //  getString(1) return second column data
+                //  getInt(0) if data is an integer value
+                tasks.add(cursor.getString(1));
+            } while (cursor.moveToNext());
+        }
+        // Close connection
+        cursor.close();
+        db.close();
+
+        return tasks;
+    }
+
+
     // Start version with 1
     // increment by 1 whenever db schema changes.
     private static final int DATABASE_VER = 1;
